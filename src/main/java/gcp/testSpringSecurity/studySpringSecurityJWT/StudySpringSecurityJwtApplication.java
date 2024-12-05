6d2a8c1474d03f066bf03637fbbbac935a807514
Gabriel Paiao <costapaiao@hotmail.com>
2024-12-05 15:13:25 -0300

    Config application.properties

---
+++ src/main/java/gcp/testSpringSecurity/studySpringSecurityJWT/StudySpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package gcp.testSpringSecurity.studySpringSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gcp/testSpringSecurity/studySpringSecurityJWT/StudySpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package gcp.testSpringSecurity.studySpringSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
